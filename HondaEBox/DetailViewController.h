17bfd2882c1e20942d85f7833ccc854700355036
cloudpower <cloudpower@cloudpowerdeMacBook-Pro.local>
2014-05-04 17:30:30 +0800

    Initial Commit

---
+++ HondaEBox/DetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.h
+//  HondaEBox
+//
+//  Created by cloudpower on 14-5-4.
+//  Copyright (c) 2014年 cloudPower. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController <UISplitViewControllerDelegate>
+
+@property (strong, nonatomic) id detailItem;
+
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+@end
---
+++ HondaEBox/MasterViewController.h
@@ -0,0 +1,17 @@
+//
+//  MasterViewController.h
+//  HondaEBox
+//
+//  Created by cloudpower on 14-5-4.
+//  Copyright (c) 2014年 cloudPower. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@end
